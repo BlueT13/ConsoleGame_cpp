e16d62bb9b12b3b7d5bd9621a64f649a5f734da9
unknown <alex23456@naver.com>
2023-12-18 16:47:17 +0900

    [Add] StringToNumber, NumberToString

---
+++ AssortrockTasks/NumberToString/NumberToString.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int StringCount(const char* const _Ptr)
+{
+	int Count = 0;
+	while (_Ptr[Count])
+	{
+		++Count;
+	}
+	return Count;
+}
+
+void NumberToString(int _Number, char* _Ptr)
+{
+	int count = 0;
+	int num = _Number;
+	while (num > 0)
+	{
+		num /= 10;
+		count++;
+	}
+
+	for (int i = 0; i < count; i++)
+	{
+		int divisor = pow(10, count - 1 - i);
+		int remainder = _Number / divisor;
+		_Number %= divisor;
+		_Ptr[i] = remainder + '0';
+
+		cout << _Ptr[i];
+	}
+}
+
+int main()
+{
+	char Arr[100] = {};
+
+	NumberToString(123456, Arr);
+}
---
+++ AssortrockTasks/StringToNumber/StringToNumber.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int StringCount(const char* const _Ptr)
+{
+	int Count = 0;
+	while (_Ptr[Count])
+	{
+		++Count;
+	}
+	return Count;
+}
+
+int StringToNumber(const char* const _NumberString)
+{
+	// 글자 개수를 알아내야 합니다.
+	// StringCount();
+	// * 10을 써야 합니다.
+	// / 써야 합니다.
+	// for문을 사용해야합니다.
+	// 예외처리 안합니다.
+
+	// char Ch = _NumberString[0];
+	int count = StringCount(_NumberString);
+	int value = 1;
+
+	for (int i = 0; i < count - 1; i++)
+	{
+		value *= 10;
+	}
+
+	int result = 0;
+
+	for (int i = 0; i < count; i++)
+	{
+		char Ch = _NumberString[i];
+		int number = Ch - '0';
+		result += number * value;
+		value /= 10;
+	}
+
+	return result;
+
+	/*int sum = 0;
+	for (int i = 0; i < count; i++)
+	{
+		char Ch = _NumberString[i];
+		sum += (Ch - '0') * pow(10, count - 1 - i);
+	}
+
+	return sum;*/
+}
+
+int main()
+{
+	int Number = StringToNumber("123456789");
+	cout << Number;
+}
