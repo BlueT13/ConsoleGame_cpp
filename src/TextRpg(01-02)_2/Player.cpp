c432b4f551f4cd7ed3d8d50ebd9eeee30fe68096
BlueT13 이태환 <alex23456@naver.com>
2024-01-02 16:31:57 +0900

    [Modify] Player.cpp 코드 순서, 주석 수정

--- src/TextRpg(01-02)_2/Player.cpp
+++ src/TextRpg(01-02)_2/Player.cpp
@@ -34,26 +34,21 @@ void Player::FightEnd(FightUnit& _Other)
 {
 	int Gold = _Other.GetGold();
 	printf_s("플레이어가 %d 골드를 벌었습니다.\n", Gold);
-	printf_s("플레이어가 %d 경험치를 얻었습니다.\n", _Other.GetExp());
 	AddGold(Gold);
 
 	// 몬스터가 준 경험치죠 _Other.GetExp();
 	// AddExp(_Other.GetExp());
 	// 나의 경험치 GetExp();
+	printf_s("플레이어가 %d 경험치를 얻었습니다.\n", _Other.GetExp());
 	CurExp += _Other.GetExp();
+
+	// LevelUpExp 비교를 해서
 	while (CurExp >= LevelUpExp)
 	{
 		CurExp = CurExp - LevelUpExp;
 		Level++;
+		// 나의 능력치를 얼마나 상승시킬지.
+		// 다 기획적인 거죠. 상승만 시키세요.
 		LevelUp();
 	}
-
-	// 나의 능력치를 얼마나 상승시킬지.
-	// 다 기획적인 거죠. 상승만 시키세요.
-
-	// LevelUpExp 비교를 해서
-
-
-
-	// Monster
 }
