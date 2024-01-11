f341416944072091f3049c18cb9b3f290f4a7c54
BlueT13 이태환 <alex23456@naver.com>
2024-01-11 13:57:14 +0900

    [Add] CList 프로젝트

---
+++ src/CList/CList.cpp
@@ -0,0 +1,239 @@
+﻿// CList.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <list>
+#include <ConsoleEngine/EngineDebug.h>
+
+typedef int DataType;
+
+// 노드형태입니다.
+class MyList
+{
+	// Node가 데이터를 1개 받기 때문입니다.
+// 더미노드라는 방식을 사용할 겁니다.
+private:
+	class ListNode
+	{
+	public:
+		DataType Data = DataType();
+		ListNode* Next = nullptr;
+		ListNode* Prev = nullptr;
+	};
+
+
+public:
+	class iterator
+	{
+		// class MyList전방 선언과 동시에 friend를 건건데
+		// mylist가 뭔지 알기 때문에.
+		friend MyList;
+
+	public:
+		iterator()
+		{
+		}
+
+		iterator(ListNode* _CurNode)
+			: CurNode(_CurNode)
+		{
+		}
+		
+		bool operator!=(const iterator& _Other)
+		{
+			return CurNode != _Other.CurNode;
+		}
+
+		DataType& operator*()
+		{
+			return CurNode->Data;
+		}
+
+		// 연산자 겹지정 중에 
+		void operator++()
+		{
+			CurNode = CurNode->Next;
+		}
+
+
+	private:
+		ListNode* CurNode = nullptr;
+	};
+
+
+	MyList()
+	{
+		Start->Next = End;
+		End->Prev = Start;
+	}
+
+	~MyList()
+	{
+		ListNode* CurNode = Start->Next;  // Start는 더미 노드이므로 실제 데이터가 있는 첫 번째 노드부터 시작
+
+		while (CurNode != End)
+		{
+			ListNode* NextNode = CurNode->Next;
+			delete CurNode;
+			CurNode = NextNode;
+		}
+
+		// 마지막으로 더미 노드를 삭제
+		delete Start;
+		delete End;
+	}
+
+	iterator begin()
+	{
+		return iterator(Start->Next);
+	}
+
+	iterator end()
+	{
+		return iterator(End);
+	}
+
+	// End의 Prev에 새로운 데이터를 넣겠다.
+	void push_back(const DataType& _Data)
+	{
+		ListNode* NewNode = new ListNode();
+		NewNode->Data = _Data;
+
+		NewNode->Next = End;
+		NewNode->Prev = End->Prev;
+
+		ListNode* PrevNode = NewNode->Prev;
+		ListNode* NextNode = NewNode->Next;
+
+		PrevNode->Next = NewNode;
+		NextNode->Prev = NewNode;
+	}
+
+	// Start의 Next에 새로운 데이터를 넣겠다.
+	void push_front(const DataType& _Data)
+	{
+		// 역함수
+		ListNode* NewNode = new ListNode();
+		NewNode->Data = _Data;
+
+		NewNode->Prev = Start;
+		NewNode->Next = Start->Next;
+
+		ListNode* PrevNode = NewNode->Prev;
+		ListNode* NextNode = NewNode->Next;
+
+		PrevNode->Next = NewNode;
+		NextNode->Prev = NewNode;
+
+	}
+
+	iterator erase(iterator& _Iter)
+	{
+		if (_Iter.CurNode == Start)
+		{
+			MsgBoxAssert("Start를 삭제하려고 했습니다.");
+		}
+
+		if (_Iter.CurNode == End)
+		{
+			MsgBoxAssert("End를 삭제하려고 했습니다.");
+		}
+
+		iterator ReturnIter;
+
+		if (nullptr != _Iter.CurNode)
+		{
+			// 다음 노드를 리턴
+			ReturnIter = iterator(_Iter.CurNode->Next);
+
+			ListNode* PrevNode = _Iter.CurNode->Prev;
+			ListNode* NextNode = _Iter.CurNode->Next;
+
+			PrevNode->Next = NextNode;
+			NextNode->Prev = PrevNode;
+
+			// 삭제하기 전에
+			// 삭제한다는것은 데이터를 전부다 지우겠다는 건데.
+			// 지운걸 사용할수 없다.
+			if (nullptr == _Iter.CurNode)
+			{
+				delete _Iter.CurNode;
+				_Iter.CurNode = nullptr;
+			}
+		}
+
+		return ReturnIter;
+	}
+
+
+protected:
+
+private:
+
+	ListNode* Start = new ListNode();
+	ListNode* End = new ListNode();
+};
+
+int main()
+{
+	LeakCheck;
+
+	{
+		std::cout << "std 리스트" << std::endl;
+		std::list<int> NewList = std::list<int>();
+		for (int i = 0; i < 5; i++)
+		{
+			NewList.push_back(i);
+			// NewList.push_front();
+		}
+
+		std::list<int>::iterator StartIter = NewList.begin();
+		std::list<int>::iterator EndIter = NewList.end();
+
+		// 지워진 노드의 다음 노드를 리턴합니다.
+		StartIter = NewList.erase(StartIter);
+
+		for (/*std::list<int>::iterator StartIter = NewList.begin()*/
+			; StartIter != EndIter
+			; ++StartIter)
+		{
+			std::cout << *StartIter << std::endl;
+			// std::cout << StartIter.operator*() << std::endl;
+		}
+	}
+
+	{
+		std::cout << "내 리스트" << std::endl;
+		MyList NewList = MyList();
+		for (int i = 0; i < 10; i++)
+		{
+			NewList.push_front(i);
+			// NewList.push_front();
+		}
+
+		MyList::iterator StartIter = NewList.begin();
+		MyList::iterator EndIter = NewList.end();
+
+		StartIter = NewList.erase(StartIter);
+
+		for (/*std::list<int>::iterator StartIter = NewList.begin()*/
+			; StartIter != EndIter
+			; ++StartIter
+			)
+		{
+			std::cout << *StartIter << std::endl;
+			// std::cout << StartIter.operator*() << std::endl;
+		}
+	}
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
