daaeab076440105ef43e46f2d935e35a60da1fa6
BlueT13 이태환 <alex23456@naver.com>
2024-01-15 16:24:36 +0900

    [Modify] Galaga  - 몬스터 좌우 이동 구현

--- src/Galaga/Monster.cpp
+++ src/Galaga/Monster.cpp
@@ -1,9 +1,10 @@
 #include "Monster.h"
 #include "ContentsEnum.h"
+#include <ConsoleEngine/EngineCore.h>
 
 
 void Monster::Update()
-{
+{	
 	ConsoleObject* CollisionObject = Collision(GalagaUpdateType::Bullet);
 
 	if (nullptr != CollisionObject)
@@ -12,4 +13,16 @@ void Monster::Update()
 		CollisionObject->Destroy();
 	}
 
+
+	if (0 >= MoveCount)
+	{
+		AddPos(Down);
+		Direction.X *= -1;
+		MoveCount = 10;
+	}
+	else
+	{
+		AddPos(Direction);
+		MoveCount--;
+	}
 }
--- src/Galaga/Monster.h
+++ src/Galaga/Monster.h
@@ -5,5 +5,9 @@ class Monster : public ConsoleObject
 {
 protected:
 	void Update() override;
+
+private:
+	int MoveCount = 10;
+	int2 Direction = { 1, 0 };
 };
 
